4e624e11b576ae938c11b6a494bd5d45f1d44d9c
gongcq <gong.cq@outlook.com>
2020-10-08 21:29:32 +0800

    1

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import os
 import torch
 import utils.data as data
 import utils.metric as metric
+import utils.evaluate as evaluate
 from model.emb_seq_bert import EmbSeqBert
 from model.seq_label import SeqLabel
 
@@ -14,11 +15,14 @@ dsm = data.DataSetManager(path='./data/tiny_data/%s' % format,
 train_data_set = dsm.train_data_set
 met = metric.Metric(label_set=train_data_set.label_tokenizer.label_set,
                     tag_set=train_data_set.tag_set, format=format)
+
 emb_seq_model = EmbSeqBert(bert_model_path=bert_model_path,
                            label_num=train_data_set.label_tokenizer.label_num,
                            trainable=True)
 seq_label_model = SeqLabel(emb_seq_model=emb_seq_model,
                            label_num=train_data_set.label_tokenizer.label_num)
+
+evaluator = evaluate.Evaluator(model=seq_label_model, data_set_manager=dsm, metric=met)
 
 for i in range(50):
     c = 0
@@ -33,22 +37,7 @@ for i in range(50):
                                            mask=seq_ids_mask_batch_t)
         print('\n~~ loss %s' % str(loss))
 
-        path_score, predict_label_ids_batch = seq_label_model(seq_ids=seq_ids_batch_t,
-                                                              mask=seq_ids_mask_batch_t)
-        seq_batch = train_data_set.decode_seq_batch(padded_seq_ids_batch=seq_ids_batch,
-                                                    seq_ids_mask_batch=seq_ids_mask_batch)
-        label_batch = train_data_set.decode_label_batch(padded_label_ids_batch=label_ids_batch,
-                                                        label_ids_mask_batch=label_ids_mask_batch)
-        predict_label_batch = train_data_set.decode_label_batch(padded_label_ids_batch=predict_label_ids_batch,
-                                                                label_ids_mask_batch=label_ids_mask_batch)
-        total_accurate, total_recall, accurate_dict, recall_dict = \
-            met.label_wise_metric_batch(true_label_list_batch=label_batch, predict_label_list_batch=predict_label_batch)
-        total_accurate_2, total_recall_2, accurate_dict_2, recall_dict_2 = \
-            met.entity_wise_metric_batch(true_label_list_batch=label_batch, predict_label_list_batch=predict_label_batch)
-        print('\n~~ label  wise: prc %.6f, rec %.6f' % (total_accurate, total_recall))
-        print('~~ entity wise: prc %.6f, rec %.6f' % (total_accurate_2, total_recall_2))
-        print('\n~~ label  wise: \nprc_detail %s \nrec_detail %s' % (accurate_dict, recall_dict))
-        print('~~ entity wise: \nprc_detail %s \nrec_detail %s' % (accurate_dict_2, recall_dict_2))
+        evaluator.eval(seq_ids_batch, label_ids_batch, seq_ids_mask_batch, label_ids_mask_batch)
 
         c += 1
 
--- utils/evaluate.py
+++ utils/evaluate.py
@@ -1,3 +1,4 @@
+import torch
 from utils.data import DataSetManager
 from utils.metric import Metric
 
@@ -6,6 +7,35 @@ class Evaluator:
         self.model = model
         self.data_set_manager = data_set_manager
         self.metric = metric
+
+    def eval(self, seq_ids_batch, label_ids_batch, seq_ids_mask_batch, label_ids_mask_batch, print_detail=True):
+        seq_ids_batch_t = torch.Tensor(seq_ids_batch).to(torch.int64)
+        seq_ids_mask_batch_t = torch.Tensor(seq_ids_mask_batch).to(torch.bool)
+
+        path_score, predict_label_ids_batch = self.model(seq_ids=seq_ids_batch_t, mask=seq_ids_mask_batch_t)
+        # seq_batch = \
+        #     self.data_set_manager.train_data_set.decode_seq_batch(padded_seq_ids_batch=seq_ids_batch,
+        #                                                           seq_ids_mask_batch=seq_ids_mask_batch)
+        label_batch = \
+            self.data_set_manager.train_data_set.decode_label_batch(padded_label_ids_batch=label_ids_batch,
+                                                                    label_ids_mask_batch=label_ids_mask_batch)
+        predict_label_batch = \
+            self.data_set_manager.train_data_set.decode_label_batch(padded_label_ids_batch=predict_label_ids_batch,
+                                                                    label_ids_mask_batch=label_ids_mask_batch)
+
+        total_accurate, total_recall, accurate_dict, recall_dict = \
+            self.metric.label_wise_metric_batch(true_label_list_batch=label_batch,
+                                                predict_label_list_batch=predict_label_batch)
+        total_accurate_2, total_recall_2, accurate_dict_2, recall_dict_2 = \
+            self.metric.entity_wise_metric_batch(true_label_list_batch=label_batch,
+                                                 predict_label_list_batch=predict_label_batch)
+
+        print('\n~~ label  wise: prc %.6f, rec %.6f' % (total_accurate, total_recall))
+        print('~~ entity wise: prc %.6f, rec %.6f' % (total_accurate_2, total_recall_2))
+
+        if print_detail:
+            print('\n~~ label  wise: \nprc_detail %s \nrec_detail %s' % (accurate_dict, recall_dict))
+            print('~~ entity wise: \nprc_detail %s \nrec_detail %s' % (accurate_dict_2, recall_dict_2))
 
     def random_eval(self, size):
         pass
