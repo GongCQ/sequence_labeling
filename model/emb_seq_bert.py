9739b7ea4ff4922557269ccbdd6bc6ef04f5a91e
gongcq <gong.cq@outlook.com>
2020-12-15 16:02:57 +0800

    1

--- model/emb_seq_bert.py
+++ model/emb_seq_bert.py
@@ -1,11 +1,12 @@
 import os
 import torch.nn as nn
 import pytorch_pretrained_bert as torch_bert
-from utils.config import USE_GPU, BERT_TRAINABLE, BERT_LAYER_INDEX, BERT_LEARNING_RATE, BERT_WEIGHT_DECAY
-from utils.config import BERT_FULL_CONN_DROP_OUT, BERT_FULL_CONN_LEARNING_RATE, BERT_FULL_CONN_WEIGHT_DECAY
+from utils.config import USE_GPU
 
 class EmbSeqBert(nn.Module):
-    def __init__(self, bert_model_path, label_num):
+    def __init__(self, bert_model_path, label_num, bert_trainable=True, bert_layer_index=-1,
+                 bert_full_conn_dropout=0, bert_learning_rate=1e-5, bert_weight_decay=0,
+                 bert_full_conn_learning_rate=0.003, bert_full_conn_weight_decay=0):
         '''
         :param bert_model_path: the path of the folder which contains bin file, not the path of the bin file.
         :param label_num: the number of labels, exclude the begin and end label.
@@ -13,11 +14,18 @@ class EmbSeqBert(nn.Module):
         super(EmbSeqBert, self).__init__()
         self.bert_model_path = bert_model_path
         self.label_num = label_num
+        self.bert_trainable = bert_trainable
+        self.bert_layer_index = bert_layer_index
+        self.bert_full_conn_dropout = bert_full_conn_dropout
+        self.bert_learning_rate = bert_learning_rate
+        self.bert_weight_decay = bert_weight_decay
+        self.bert_full_conn_learning_rate = bert_full_conn_learning_rate
+        self.bert_full_conn_weight_decay = bert_full_conn_weight_decay
         self.bert_model = torch_bert.BertModel.from_pretrained(bert_model_path)
-        self.dropout = nn.Dropout(p=BERT_FULL_CONN_DROP_OUT)
+        self.dropout = nn.Dropout(p=bert_full_conn_dropout)
         self.full_conn = nn.Linear(self.bert_model.config.hidden_size, label_num + 2)
         for param in self.bert_model.parameters():
-            param.requires_grad  = BERT_TRAINABLE
+            param.requires_grad_(bert_trainable)
 
         if USE_GPU:
             self.bert_model = nn.DataParallel(self.bert_model)
@@ -31,16 +39,16 @@ class EmbSeqBert(nn.Module):
         :param mask: tensor, shape [batch_size * seq_len], with mask-indicator selected in [0, 1]
         :return: tensor, shape [batch_size, seq_len, emb_size], while emb_size is the hidden_size of bert_model, 768.
         '''
-        if BERT_LAYER_INDEX == -1:
+        if self.bert_layer_index == -1:
             char_emb_seq, sen_emb = self.bert_model(input_ids=seq_ids, attention_mask=mask,
                                                     output_all_encoded_layers=False)
         else:
             char_emb_seq_list, sen_emb = self.bert_model(input_ids=seq_ids, attention_mask=mask,
                                                          output_all_encoded_layers=True)
-            char_emb_seq = char_emb_seq_list[BERT_LAYER_INDEX]
+            char_emb_seq = char_emb_seq_list[self.bert_layer_index]
 
-        if BERT_FULL_CONN_DROP_OUT > 0:
-            feature_seq = self.full_conn(self.dropout(char_emb_seq) * (1 - BERT_FULL_CONN_DROP_OUT))
+        if self.bert_full_conn_dropout > 0:
+            feature_seq = self.full_conn(self.dropout(char_emb_seq) * (1 - self.bert_full_conn_dropout))
         else:
             feature_seq = self.full_conn(char_emb_seq)
         # print('char_emb_seq norm     %s' % str(float(char_emb_seq.abs().mean())))
@@ -51,10 +59,10 @@ class EmbSeqBert(nn.Module):
 
     def get_params_config(self):
         params_config = [{'params': self.full_conn.parameters(),
-                          'lr': BERT_FULL_CONN_LEARNING_RATE,
-                          'weight_decay': BERT_FULL_CONN_WEIGHT_DECAY}]
-        if BERT_TRAINABLE:
+                          'lr': self.bert_full_conn_learning_rate,
+                          'weight_decay': self.bert_full_conn_weight_decay}]
+        if self.bert_trainable:
             params_config += [{'params': self.bert_model.parameters(),
-                               'lr': BERT_LEARNING_RATE,
-                               'weight_decay': BERT_WEIGHT_DECAY}]
+                               'lr': self.bert_learning_rate,
+                               'weight_decay': self.bert_weight_decay}]
         return params_config
